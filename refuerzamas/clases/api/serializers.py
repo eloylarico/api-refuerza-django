549ececdf825bb1c83047f5a2d9a6f84ca50e961
Jahir Moncada <geminis31_98@hotmail.com>
2020-10-18 21:45:24 -0500

    Serializer para front en docentes

--- refuerzamas/clases/api/serializers.py
+++ refuerzamas/clases/api/serializers.py
@@ -135,10 +135,14 @@ class UserTutorModelSerializer(serializers.ModelSerializer):
     perfil_tutor = TutorModelSerializer(required=False)
     display_name = serializers.CharField(read_only=True)
 
+    genero_id = serializers.IntegerField(required=False, allow_null=True)
+
     class Meta:
         model = User
         fields = (
             "id",
+            "first_name",
+            "last_name",
             "username",
             "display_name",
             "nickname",
@@ -150,6 +154,7 @@ class UserTutorModelSerializer(serializers.ModelSerializer):
             "direccion",
             "observaciones",
             "genero",
+            "genero_id",
             "perfil_tutor",
         )
         read_only_fields = (
@@ -181,7 +186,7 @@ class EstudianteModelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Estudiante
-        fields = ("institucion", "grado", "tutor", "institucion_id", "grado_id")
+        fields = ("institucion", "grado", "tutor", "institucion_id", "breve_cv", "grado_id", "estrellas")
         read_only_fields = ("tutor",)
 
 
@@ -209,6 +214,7 @@ class UserEstudianteModelSerializer(serializers.ModelSerializer):
             "genero",
             "perfil_estudiante",
             "genero_id",
+            "display_name"
         )
         read_only_fields = (
             "email",
@@ -239,6 +245,7 @@ class UserEstudianteModelSerializer(serializers.ModelSerializer):
         if data:
             estudiante.institucion_id = data.get("institucion_id", estudiante.institucion_id)
             estudiante.grado_id = data.get("grado_id", estudiante.grado_id)
+            estudiante.breve_cv = data.get("breve_cv", estudiante.breve_cv)
             estudiante.save()
         return instance
 
@@ -248,10 +255,14 @@ class UserDocenteModelSerializer(serializers.ModelSerializer):
     perfil_docente = DocenteModelSerializer(required=False)
     display_name = serializers.CharField(read_only=True)
 
+    genero_id = serializers.IntegerField(required=False, allow_null=True)
+
     class Meta:
         model = User
         fields = (
             "id",
+            "first_name",
+            "last_name",
             "username",
             "display_name",
             "nickname",
@@ -264,6 +275,7 @@ class UserDocenteModelSerializer(serializers.ModelSerializer):
             "observaciones",
             "genero",
             "perfil_docente",
+            "genero_id"
         )
         read_only_fields = (
             "email",
@@ -278,6 +290,8 @@ class UserDocenteModelSerializer(serializers.ModelSerializer):
         if validated_data.get("perfil_docente"):
             data = validated_data.get("perfil_docente")
 
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
         instance.username = validated_data.get("username", instance.username)
         instance.nickname = validated_data.get("nickname", instance.nickname)
         instance.avatar = validated_data.get("avatar", instance.avatar)
@@ -285,6 +299,8 @@ class UserDocenteModelSerializer(serializers.ModelSerializer):
         instance.celular = validated_data.get("celular", instance.celular)
         instance.direccion = validated_data.get("direccion", instance.direccion)
         instance.observaciones = validated_data.get("observaciones", instance.observaciones)
+        instance.genero_id = validated_data.get("genero_id", instance.genero_id)
+        instance.save()
 
         if data:
             docente.grado_instruccion_id = data.get("grado_instruccion_id", docente.grado_instruccion_id)
@@ -328,6 +344,7 @@ class ClaseModelSerializer(serializers.ModelSerializer):
 
 class MensajeModelSerializer(serializers.ModelSerializer):
     tutor = serializers.SerializerMethodField()
+
     class Meta:
         model = Mensaje
         fields = "__all__"
@@ -340,9 +357,11 @@ class MensajeModelSerializer(serializers.ModelSerializer):
 
 
 class UserModelSerializer(serializers.ModelSerializer):
+    perfil_tutor = TutorModelSerializer()
+
     class Meta:
         model = User
-        fields = ["id", "display_name", "avatar"]
+        fields = ["id", "display_name", "avatar", "tipo_usuario", "perfil_tutor"]
 
 
 class ChatModelSerializer(serializers.ModelSerializer):
